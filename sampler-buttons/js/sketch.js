2424096fc33927f8875f23f8cf8967cfdbe27de0
Erin Demastes <85418446+edemastes@users.noreply.github.com>
2024-01-23 15:49:55 -0600

    Update sketch.js

--- sampler-buttons/js/sketch.js
+++ sampler-buttons/js/sketch.js
@@ -24,8 +24,7 @@ function setup() {
 function draw() {
   background(239, 169, 218);
   textSize(16);
-  textAlign(CENTER, TOP);
-  text("Press buttons for sound!");
+  text("Press buttons for sound!",CENTER, TOP);
 }
 
 
