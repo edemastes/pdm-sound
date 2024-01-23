3f6d5eba2e31f089b667ea5b82143e0d6ffb328c
Erin Demastes <85418446+edemastes@users.noreply.github.com>
2024-01-23 10:06:38 -0600

    Update sketch.js

--- Tone-Template/sketch.js
+++ Tone-Template/sketch.js
@@ -1,30 +1,7 @@
-let soundFX;
-
-function preload () {
-  soundFX = new Tone.Players({
-    popcorn: "Popcorn+Maker.mp3", 
-    water: "Water+Trickle.mp3"
-  }).toDestination();
-}
-
-function keyPressed() { 
-  if (keyCode === 81) {
-    //q
-    soundFX.player("popcorn").start(); 
-  } else if (keyCode === 87) {
-    //w
-    soundFX.player("water").start();
-  }
-}
-
-
 function setup() {
-  createCanvas(1800, 1600);
-  
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  console.log (keyCode);
 }
-  
