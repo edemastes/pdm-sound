e9b5011c292752476c8b6a61ecc8c5e8e74b2eec
Erin Demastes <85418446+edemastes@users.noreply.github.com>
2024-01-23 15:35:51 -0600

    Add files via upload

---
+++ sampler-keypressed/sketch.js
@@ -0,0 +1,30 @@
+let soundFX;
+
+function preload () {
+  soundFX = new Tone.Players({
+    popcorn: "Popcorn+Maker.mp3", 
+    water: "Water+Trickle.mp3"
+  }).toDestination();
+}
+
+function keyPressed() { 
+  if (keyCode === 81) {
+    //q
+    soundFX.player("popcorn").start(); 
+  } else if (keyCode === 87) {
+    //w
+    soundFX.player("water").start();
+  }
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(255, 204, 0);
+  text("Press Q or W for sound!", 50, 50);
+}
+
+function draw() {
+  console.log (keyCode);
+}
+  
